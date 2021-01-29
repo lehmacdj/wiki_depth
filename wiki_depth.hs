b69da85d47f19492cc01017cec85cf9aeb8ba15a
Devin Lehmacher <lehmacdj@gmail.com>
2021-01-28 20:28:01 -0800

    changes that compile and do stuff, not sure what they were because they just existed

--- wiki_depth.hs
+++ wiki_depth.hs
@@ -1,27 +1,29 @@
-import Control.Monad
-import Network.HTTP.Conduit
+module Main where
+
+import Network.HTTP.Conduit hiding (path)
 import System.Environment
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as C
-import qualified Data.Text as T
 import Text.HTML.TagSoup
 import Data.List
+import Text.StringLike
 
+main :: IO ()
 main = do
     args <- getArgs
     if length args == 1
         then do
-            let first = args !! 0
-            (cycle, path) <- findCycle first [first]
+            let first = head args
+            (cycle', path) <- findCycle first [first]
             let path_length  = show $ length path
             putStrLn $ first
                 ++ " has a path of length "
                 ++ path_length
                 ++ ":"
             printListIndented path
-            let cycle_length = show $ length cycle
+            let cycle_length = show $ length cycle'
             putStrLn $ "with a cycle of length " ++ cycle_length ++ ":"
-            printListIndented cycle
+            printListIndented cycle'
         else
             putStrLn "Usage: ./wiki_depth <wikipedia-page>"
 
@@ -32,12 +34,12 @@ printListIndented list =
 -- Returns the cycle and the path to the cycle
 findCycle :: String -> [String] -> IO ([String], [String])
 findCycle url list = do
-    text <- fetchPage (url)
+    text <- fetchPage url
     let first = head $ getLinkReferences $ getBody text
     if first `elem` list
-        then return $ ( reverse $ first : takeWhile (/=first) list
-                      , reverse $ dropWhile (/=first) list
-                      )
+        then pure ( reverse $ first : takeWhile (/=first) list
+                    , reverse $ dropWhile (/=first) list
+                    )
         else findCycle first (first : list)
 
 
@@ -45,13 +47,15 @@ fetchPage :: String -> IO L.ByteString
 fetchPage article = simpleHttp ("https://en.wikipedia.org/wiki/" ++ article)
 
 -- getLinkReferences :: L.ByteString -> [String]
+getLinkReferences :: (StringLike a, Show a) => [Tag a] -> [String]
 getLinkReferences tags =
     filter (not . (':' `elem`)) $
     map (drop 6) $
     filter ("/wiki/" `isPrefixOf`) $
     map (unQuote . show . (\(TagOpen _ ((_,href):_)) -> href) . head) $
-    partitions (~== "<a>") $ tags
+    partitions (~== "<a>") tags
 
+getBody :: C.ByteString -> [Tag C.ByteString]
 getBody text =
     removeCoordinateReferences $
     findBottomLevelPTag [] $
@@ -69,12 +73,13 @@ findBottomLevelPTag [] (t:ts) =
     case t of TagOpen n _
                           | n == C.pack "p" -> ts
                           | otherwise -> findBottomLevelPTag [n] ts
-              e -> findBottomLevelPTag [] ts
+              _ -> findBottomLevelPTag [] ts
 findBottomLevelPTag s@(x:xs) (t:ts)
     | TagOpen x [] ~== t = findBottomLevelPTag (x:s) ts
     | TagClose x ~== t = findBottomLevelPTag xs ts
     | otherwise = findBottomLevelPTag s ts
 
+removeCoordinateReferences :: StringLike a => [Tag a] -> [Tag a]
 removeCoordinateReferences tags =
     if tags !! 1 ~== "<span id=coordinates>"
         then dropWhile (~/= "<p>") tags
